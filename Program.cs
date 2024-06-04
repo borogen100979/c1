c9d11cebc7d61e50add3846f699d43c1d22838e6
borogen100979 <139426516+borogen100979@users.noreply.github.com>
2024-06-04 19:16:55 +0300

    1

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿{
+int J=0;
+    Console.Write("Введите x}: ");
+        Double x = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Введите y}: ");
+        Double y = Convert.ToDouble(Console.ReadLine());
+    if (x == 0 || y == 0) J = 0;
+    else if (y > 0.0 && x > 0.0) J = 1;
+    else if (y > 0.0 && x < 0.0) J = 2;
+    else if (y < 0.0 && x < 0.0) J = 3;
+    else if (y < 0.0 && x > 0.0) J = 4;
+  
+Console.WriteLine(J);
+}
